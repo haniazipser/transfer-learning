fb8a7b0b3e807184a1fead654ffba48471e715f7
Hania <haniazipser2004@gmail.com>
2026-04-30 11:15:37 +0200

    feat: add augmentation

--- src/data/datamodule.py
+++ src/data/datamodule.py
@@ -11,10 +11,20 @@ from .KaggleTestDataset import KaggleTestDataset
 MEAN = (0.485, 0.456, 0.406)
 STD = (0.229, 0.224, 0.225)
 
-TRANSFORM = transforms.Compose([
+EVAL_TRANSFORM = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor(),
     transforms.Normalize(MEAN, STD),
+])
+
+TRAIN_TRANSFORM = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.RandomHorizontalFlip(p=0.5),
+    transforms.RandomVerticalFlip(p=0.5),
+    transforms.RandomRotation(degrees=180),
+    transforms.ToTensor(),
+    transforms.Normalize(MEAN, STD),
+    transforms.RandomErasing(p=0.2, scale=(0.02, 0.15)),
 ])
 
 
@@ -24,20 +34,26 @@ class DataModule:
         train_csv = data_dir / "train.csv"
         test_csv = data_dir / "test.csv"
 
-        full_train = KaggleDataset(
+        full_train_aug = KaggleDataset(
             csv_file=str(train_csv),
             data_dir=str(data_dir),
-            transform=TRANSFORM,
+            transform=TRAIN_TRANSFORM,
         )
 
-        val_size = int(len(full_train) * config.val_split)
-        train_size = len(full_train) - val_size
-
-        train_dataset, val_dataset = random_split(
-            full_train,
-            [train_size, val_size],
-            generator=torch.Generator().manual_seed(config.seed),
+        full_train_eval = KaggleDataset(
+            csv_file=str(train_csv),
+            data_dir=str(data_dir),
+            transform=EVAL_TRANSFORM,
         )
+
+        val_size = int(len(full_train_aug) * config.val_split)
+        train_size = len(full_train_aug) - val_size
+
+        generator = torch.Generator().manual_seed(config.seed)
+        indices = torch.randperm(len(full_train_aug), generator=generator).tolist()
+
+        train_dataset = torch.utils.data.Subset(full_train_aug, indices[:train_size])
+        val_dataset = torch.utils.data.Subset(full_train_eval, indices[train_size:])
 
         self.train_loader = DataLoader(
             train_dataset,
@@ -55,12 +71,12 @@ class DataModule:
             pin_memory=True,
         )
 
-        self.num_classes = len(full_train.labels)
+        self.num_classes = len(full_train_aug.labels)
 
         test = KaggleTestDataset(
             csv_file=str(test_csv),
             data_dir=str(data_dir),
-            transform=TRANSFORM,
+            transform=EVAL_TRANSFORM,
         )
 
         self.test_loader = DataLoader(
@@ -68,4 +84,3 @@ class DataModule:
             batch_size=config.batch_size,
             shuffle=False,
         )
-
